2c13e3bb6d93ae1e874111c77f12c80891b687b6
Goudarz Jafari <goudarz.jafari@gmail.com>
2024-02-06 02:08:32 +0330

    Add Persian translation

---
+++ src/lang/fa.ts
@@ -0,0 +1,26 @@
+export default {
+  // Months
+  January: "ژانویه",
+  February: "فوریه",
+  March: "مارس",
+  April: "آوریل",
+  May: "مه",
+  June: "ژوئن",
+  July: "ژوئیه",
+  August: "اوت",
+  September: "سپتامبر",
+  October: "اکتبر",
+  November: "نوامبر",
+  December: "دسامبر",
+
+  // Weekday
+  Sunday: "یک‌شنبه",
+  Monday: "دوشنبه",
+  Tuesday: "سه‌شنبه",
+  Wednesday: "چهارشنبه",
+  Thursday: "پنج‌شنبه",
+  Friday: "جمعه",
+  Saturday: "شنبه",
+
+  Today: "امروز",
+};
